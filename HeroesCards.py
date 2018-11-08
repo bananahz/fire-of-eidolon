fe56845a834de851392766dd63ed4cfb2fb1c5a3
othscs014 <44376894+othscs014@users.noreply.github.com>
2018-11-08 13:46:12 -0600

    Update HeroesCards.py

--- HeroesCards.py
+++ HeroesCards.py
@@ -20,90 +20,24 @@ class HeroesCards():
     aelfric = h.Hero("Aelfric", 3, 1, 2, 3, 1, 1, 1, 1, 1)
     sirus = h.Hero("Sirus", 3, 3, 2, 1, 1, 1, 1, 1, 1)
     #Send the hero attributes for scrolls, keys, and picks here
-    def specialDoorTokens(self, amt_of_scroll, amt_of_key, amt_of_pick, at_gem, at_heart, at_book):
+    def voraxTokens(self, amt_of_scroll, amt_of_key, amt_of_pick, at_gem, at_heart, at_book):
         tokens = t.Tokens(amt_of_scroll, amt_of_key, amt_of_pick)
         self.at_gem = at_gem
         self.at_heart = at_heart
         self.at_book = at_book
-        if(at_gem == True and tokens.canBreakGem()):
-            tokens.state_of_gem = tokens.broken_gem
+        if(tokens.state_of_gem == tokens.broken_gem and tokens.state_of_heart == tokens.broken_heart and tokens.state_of_book == tokens.broken_book):
+            tokens.state_of_eidolon = tokens.can_get_eidolon
         else:
-            tokens.state_of_gem = tokens.gem
-        if (at_heart == True and tokens.canBreakHeart()):
-            tokens.state_of_heart = tokens.broken_heart
-        else:
-            tokens.state_of_heart = tokens.heart
-        if (at_book == True and tokens.canBreakBook()):
-            tokens.state_of_book = tokens.broken_book
-        else:
-            tokens.state_of_book = tokens.book
+            if(at_gem == True and tokens.canBreakGem()):
+                tokens.state_of_gem = tokens.broken_gem
+            else:
+                tokens.state_of_gem = tokens.gem
+            if (at_heart == True and tokens.canBreakHeart()):
+                tokens.state_of_heart = tokens.broken_heart
+            else:
+                tokens.state_of_heart = tokens.heart
+            if (at_book == True and tokens.canBreakBook()):
+                tokens.state_of_book = tokens.broken_book
+            else:
+                tokens.state_of_book = tokens.book
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """def __init__(self):
-        self.writeHeroes = open("Heroes.txt", "w")
-        self.readHeroes = open("Heroes.txt", "r")
-
-    def heroesF(self):
-        with open("Heroes.txt", "w") as self.f:
-            self.f.write("Amelia, AP: 3, STR: 2, DEX: 2, INT: 2, BLESS, SANCTIFY\n")
-            self.f.write("Aveloc, AP: 3, STR: 1, DEX: 3, INT: 2, SOUL THIRST, CONSUME\n")
-            self.f.write("Sydney and \"Bob\", AP: 3, STR: 2, DEX: 2,INT: 2, REMOTE CONTROL, METALLIC SACRIFICE\n")
-            self.f
-            .write("Tetemeko, AP: 3, STR: 3, DEX: 2, INT: 1, GEOPIVOT, TERRAPORT\n")
-            self.f.write("Cecilia, AP: 3, STR: 1, DEX 3, INT: 2, SMITE, HOLY LIGHT\n")
-            self.f.write("Kaylana, AP: 3, STR: 2, DEX: 1, INT: 3, RANGER'S SIGHT, MASTER FALCONY\n")
-            self.f.write("Kalistos, AP: 3, STR: 3, DEX: 1, INT: 2, FIND SECRETS, SNEAK ATTACK\n")
-            self.f.write("Daga, AP: 3, STR: 2, DEX: 3, INT: 1, MINDSTRIKE, TIMETURN\n")
-            self.f.write("Cirrus, AP: 3, STR: 2, DEX: 1, INT: 3, ENORMOUS SWORD, BREAK LIMIT\n")
-            self.f.write("Carver, AP: 3, STR: 2, DEX: 2, INT: 2, MYSTIC BOOMERANG, WALLCRACK BOMBS\n")
-            self.f.write("Aelfric, AP: 3, STR: 1, DEX: 2, INT: 3, POLEARM LUNGE, HERO'S CHARGE\n")
-            self.f.write("Sirus, AP: 3, STR: 3, DEX: 2, INT: 1, OBJECT PORTAL, TELEPORT\n")
-            self.f.close()
-        print (self.readHeroes.read())
-        self.readHeroes.close()
-    def getHeroesAttri(self,hero):
-        hold = []
-        with open("Heroes.txt","r") as f:
-            chunk=[]
-            print ("hello")
-            for hero in f:
-                print ("I"+chunk)
-                chunk.append(hero)
-        return chunk"""
