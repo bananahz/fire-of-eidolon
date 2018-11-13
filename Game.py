c81b41efe02a5be8b2541afc1365b70c964b6c66
bananahz <44273288+bananahz@users.noreply.github.com>
2018-11-13 13:37:48 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,4 +1,34 @@
 # logic
+import pygame, Cards as c, Room as r, HeroesCards as h, Player as p
 
 
-class Game():
+class Game:
+
+    def __init__(self):
+
+        self.game_over = False
+
+        self.card_list = pygame.sprite.Group()
+        self.room_list = pygame.sprite.Group()
+        self.heroes_list = pygame.sprite.Group()
+
+        self.all_sprites = pygame.sprite.Group()
+
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            # mouse events
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.game_over:
+                    self.__init__()
+
+        return False
+
+    def logic(self):
+        if not self.game_over:
+            self.all_sprites.update()
+
+    def display_frame(self, screen):
+        return
+        # graphics
