a63ff01066bed7354f9e2267b335b9eff6648035
othscs014 <44376894+othscs014@users.noreply.github.com>
2018-11-26 13:14:55 -0600

    Update HeroesCards.py

--- HeroesCards.py
+++ HeroesCards.py
@@ -1,5 +1,6 @@
 import Hero as h
 import Tokens as t
+import pygame
 class HeroesCards():
     at_gem = False
     at_heart = False
@@ -7,18 +8,23 @@ class HeroesCards():
     #Hero's name, AP, STR, DEX, INT, move, explore, attack, wait
     #CHECK HERO CLASS FOR ANY TOOLS METHODS YOU MIGHT NEED
     #THIS WILL HELP SAVE TIME
-    amelia = h.Hero("Amelia", 3, 2, 3, 1, 1, 1, 1, 1, 1)
-    aveloc = h.Hero("Aveloc", 3, 1, 3, 2, 1, 1, 1, 1, 1)
-    sydney = h.Hero("Sydney and \"Bob\"", 3, 2, 2, 2, 1, 1, 1, 1, 1)
-    tetemeko = h.Hero("Tetemeko", 3, 3, 2, 1, 1, 1, 1, 1, 1)
-    cecilia = h.Hero("Cecilia", 3, 1, 3, 2, 1, 1, 1, 1, 1)
-    kaylana = h.Hero("Kaylana", 3, 2, 1, 3, 1, 1, 1, 1, 1)
-    kalistos = h.Hero("Kalistos", 3, 3, 1, 2, 1, 1, 1, 1, 1)
-    daga = h.Hero("Daga", 3, 2, 3, 1, 1, 1, 1, 1, 1)
-    cirrus = h.Hero("Cirrus", 3, 2, 1, 3, 1, 1, 1, 1, 1)
-    carver = h.Hero("Carver", 3, 2, 2, 2, 1, 1, 1, 1, 1)
-    aelfric = h.Hero("Aelfric", 3, 1, 2, 3, 1, 1, 1, 1, 1)
-    sirus = h.Hero("Sirus", 3, 3, 2, 1, 1, 1, 1, 1, 1)
+    amelia = h.Hero("Amelia", "Cleric", 3, 2, 3, 1, 1, 1, 1, 1, 1)
+    aveloc = h.Hero("Aveloc", "Darknight", 3, 1, 3, 2, 1, 1, 1, 1, 1)
+    sydney = h.Hero("Sydney", "Engineer", 3, 2, 2, 2, 1, 1, 1, 1, 1)
+    tetemeko = h.Hero("Tetemeko", "Geomancer", 3, 3, 2, 1, 1, 1, 1, 1, 1)
+    cecilia = h.Hero("Cecilia", "Paladin", 3, 1, 3, 2, 1, 1, 1, 1, 1)
+    kaylana = h.Hero("Kaylana", "Ranger", 3, 2, 1, 3, 1, 1, 1, 1, 1)
+    kalistos = h.Hero("Kalistos", "Rogue", 3, 3, 1, 2, 1, 1, 1, 1, 1)
+    daga = h.Hero("Daga", "Sage", 3, 2, 3, 1, 1, 1, 1, 1, 1)
+    cirrus = h.Hero("Cirrus", "Soldier", 3, 2, 1, 3, 1, 1, 1, 1, 1)
+    carver = h.Hero("Carver", "Swordsman", 3, 2, 2, 2, 1, 1, 1, 1, 1)
+    aelfric = h.Hero("Aelfric", "Warrior", 3, 1, 2, 3, 1, 1, 1, 1, 1)
+    sirus = h.Hero("Sirus", "Wizard", 3, 3, 2, 1, 1, 1, 1, 1, 1)
+
+    #Lol cause i thought we needed it
+    HeroList = [aelfric, amelia, aveloc, carver, cecilia, cirrus, daga,
+                kalistos, kaylana, sirus, sydney, tetemeko]
+
     #Send the hero attributes for scrolls, keys, and picks here
     def voraxTokens(self, amt_of_scroll, amt_of_key, amt_of_pick, at_gem, at_heart, at_book):
         tokens = t.Tokens(amt_of_scroll, amt_of_key, amt_of_pick)
@@ -40,4 +46,5 @@ class HeroesCards():
                 tokens.state_of_book = tokens.broken_book
             else:
                 tokens.state_of_book = tokens.book
-
+    def getImage(name):
+        return pygame.image.load("Textures/Heroes/Hero/"+name+".jpg").convert()
