effcd7fb2b54037dfdff4c6b6a2128ad84504e41
bananahz <44273288+bananahz@users.noreply.github.com>
2018-12-13 13:18:40 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,19 +1,47 @@
 # logic
 import Cards as c, Room as r, Difficulty as d, HeroesCards as h
 
-
 class Game:
     room_class = None
     map = []
     main_room = None
-    current_player = 0
-    # temporary
-    difficulty = None
+    current_player = None
+    current_card_list = []
+    current_deck = []
+    void_deck = []
+    difficulty_level = None
+    player_list = []
 
     def __init__(self):
         self.room_class = r.Room()
         self.map = [[self.room_class.all_rooms.get("Vestibule")]]
         self.main_room = (0, 0)
+        self.difficulty_level = d.All_Difficulties().difficulties.get("Normal") # random diff for now 
+        self.player_list = [h.HeroesCards.aelfric, h.HeroesCards.cecilia, h.HeroesCards.tetemeko] # random players for now 
+        self.current_player = self.player_list[0]
+        self.map[0][0].players = self.player_list
+        
+    def player_actions(self, player, action, token):
+        xPos = 0
+        yPos = 0
+        for x in range(len(self.map[0])):
+            for i in range(len(self.map[1])):
+                if self.map[x][i].players.contains(player):
+                    current_tile = self.map[x][i]
+                    xPos = x
+                    yPos = i
+
+
+        if self.current_player.amt_of_ap > 0:
+            if action == 'w':
+                if self.map[yPos + 1] > -1:
+                    self.map[xPos][yPos + 1].players.append(player)
+
+
+        # repeat for other keys
+        if action == "pick_token" and current_tile.token != None:
+            if current_tile.token == "green token":
+                return 
 
     #adds room tile to map
     def add_to_map(self, root_position, direction, room):
