f15f3136a062843d6022de0de3c80e19578364a2
yeetStreet <44377367+yeetStreet@users.noreply.github.com>
2018-12-13 13:03:40 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import os, sys
-import pygame, Game as g, HeroesGraphics as hg, Difficulty as d, HeroesCards as hc, Cards as c
+import pygame, Game as g, Difficulty as d, Cards as c
 from pygame.locals import *
 from tkinter import *
 
@@ -25,11 +25,23 @@ screen.blit(backgroundImage, (0, 0))
 clock = pygame.time.Clock()
 game = g.Game()
 cards = c.Cards()
-heroesGraphics = hg.HeroesGraphics()
+drawn_card = []
+#heroesGraphics = hg.HeroesGraphics()
 
+# button status
+pressed = False
+draw_button = False
+placing_button = False
+rotate_button = False
+
+# set positions and scales for buttons and cards
+dif_from_mouse = 0
+shift_x = 350
+shift_y = 250
+scale_of_map = (100, 100)
 
 # intro = hg.HeroesGraphics()
-# player_list,difficulty = intro.hero_Graphics()
+# player_list,difficulty = intrhero_Graphics()
 # print(player_list)
 # print(difficulty)
 
@@ -54,23 +66,30 @@ def load_sound(name):
 
 def main():
     while True:
-        # intro.hero_Graphics()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        # intrhero_Graphics()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             else:
-                heroesGraphics.hero_Graphics()
-                print(heroesGraphics.hero_Graphics()[0])
-                print(heroesGraphics.hero_Graphics()[1])
-                screen.blit(backgroundImage, (0, 0))
-                draw_hero_list()
-                run_card_graphics()
+                event_manager_map(mouse_x, mouse_y, event)
 
-                pygame.display.flip()
-                clock.tick(60)
+        # heroesGraphics.hero_Graphics()
+        # print(heroesGraphics.hero_Graphics()[0])
+        # print(heroesGraphics.hero_Graphics()[1])
+        screen.blit(backgroundImage, (0, 0))
+        # draw_hero_list()
+        draw_map()
+        run_card_graphics()
+
+        pygame.display.update()
+        clock.tick(60)
 
 
-heroes = hc.HeroesCards.HeroList  # need to change to list of players
+
+
+
+#heroes = hc.HeroesCards.HeroList  # need to change to list of players
 
 card_scale = .6
 card_width = int(1149 * card_scale)
@@ -78,7 +97,7 @@ card_height = int(749 * card_scale)
 
 turn = "Cleric"  # test value
 
-
+"""
 def draw_hero_list():
     mouse = pygame.mouse.get_pos()
     font = pygame.font.Font('freesansbold.ttf', 8)
@@ -92,6 +111,7 @@ def draw_hero_list():
         screen.blit(pygame.transform.scale(pygame.image.load("Textures/CharacterImages/" + heroes[i].type + ".png"),
                                            (40, 40)), [25, i * 50 + 35])
         screen.blit(font.render(heroes[i].type, True, (255, 255, 255)), [25, i * 50 + 28])
+        """
 
 
 cards.load()
@@ -125,6 +145,140 @@ def run_card_graphics():
         if len(cards.current_deck) == 0:
             current_deck = cards.current_deck
             print(len(cards.current_deck))
+
+
+def event_manager_map(mouse_x, mouse_y, event):
+    global pressed
+    global screen
+    global draw_button
+    global placing_button
+    global rotate_button
+
+    # set positions and scales for buttons and cards
+    global dif_from_mouse
+    global shift_x
+    global shift_y
+    global scale_of_map
+    global drawn_card
+
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+            pos = event.pos
+            if 675 <= pos[0] <= 775 and 450 <= pos[1] <= 550:
+                draw_button = True
+            elif 550 <= pos[0] <= 560 and 435 <= pos[1] <= 445 and len(drawn_card) > 0:
+                rotate_button = True
+            elif 540 <= pos[0] <= 665 and 450 <= pos[1] <= 575 and len(drawn_card) > 0:
+                placing_button = True
+            else:
+                pressed = True
+                dif_from_mouse = shift_x - mouse_x, shift_y - mouse_y
+
+    if event.type == pygame.MOUSEBUTTONUP:
+        if event.button == 1:
+            pos = event.pos
+            if 675 <= pos[0] <= 775 and 450 <= pos[1] <= 550 and len(game.room_class.current_deck) > 0 and len(
+                    drawn_card) < 1:
+                draw_button = False
+                drawn_card = game.room_class.draw()
+
+            elif 550 <= pos[0] <= 560 and 435 <= pos[1] <= 445 and len(drawn_card) > 0:
+                rotate_button = False
+                drawn_card[0].rotation()
+
+            elif len(drawn_card) > 0 and placing_button and (
+                    (540 > pos[0] or pos[0] > 665) or (450 > pos[1] or pos[1] > 575)):
+                placing_button = False
+                neg_y = 0
+                neg_x = 0
+                if ((mouse_y - shift_y) / 100) + game.main_room[0] < 0:
+                    neg_y -= 1
+                if ((mouse_x - shift_x) / 100) + game.main_room[1] < 0:
+                    neg_x -= 1
+                placed = game.add_to_map((neg_y + int(
+                    ((mouse_y - shift_y) / 100) + game.main_room[0]), neg_x + int(
+                    ((mouse_x - shift_x) / 100) + game.main_room[1])), drawn_card[0])
+                if placed:
+                    drawn_card = []
+            else:
+                pressed = False
+                draw_button = False
+
+
+def card_draw():
+    global pressed
+    global screen
+    global draw_button
+    global placing_button
+    global rotate_button
+
+    # set positions and scales for buttons and cards
+    global dif_from_mouse
+    global shift_x
+    global shift_y
+    global scale_of_map
+
+    img = game.room_class.all_rooms.get("BackOfCard").image
+
+    if len(game.room_class.current_deck) > 0:
+        if draw_button:
+            img = pygame.transform.smoothscale(img, (90, 90))
+            screen.blit(img, (680, 455))
+        else:
+            img = pygame.transform.smoothscale(img, (100, 100))
+            screen.blit(img, (675, 450))
+
+    if len(drawn_card) != 0:
+        draw = drawn_card[0].image
+        draw = pygame.transform.smoothscale(draw, (125, 125))
+        screen.blit(draw, (540, 450))
+        pygame.draw.rect(screen, (255, 255, 255), (550, 435, 10, 10))
+
+
+def draw_map():
+    global pressed
+    global screen
+    global draw_button
+    global placing_button
+    global rotate_button
+
+    # set positions and scales for buttons and cards
+    global dif_from_mouse
+    global shift_x
+    global shift_y
+    global scale_of_map
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    if pressed and not draw_button:
+        new_shift_x = dif_from_mouse[0] + mouse_x
+        new_shift_y = dif_from_mouse[1] + mouse_y
+
+        if 0 > (((len(game.map) - 1) - game.main_room[0]) * 100) + new_shift_y:
+            shift_y = -(((len(game.map) - 1) - game.main_room[0]) * 100)
+        elif ((0 - game.main_room[0]) * 100) + new_shift_y > 500:
+            shift_y = 500 - ((0 - game.main_room[0]) * 100)
+        else:
+            shift_y = new_shift_y
+
+        if 0 > (((len(game.map[0]) - 1) - game.main_room[1]) * 100) + new_shift_x:
+            shift_x = -(((len(game.map[0]) - 1) - game.main_room[1]) * 100)
+        elif ((0 - game.main_room[1]) * 100) + new_shift_x > 700:
+            shift_x = 700 - ((0 - game.main_room[1]) * 100)
+        else:
+            shift_x = new_shift_x
+
+    for y in range(len(game.map)):
+        for x in range(len(game.map[y])):
+            if game.map[y][x] is not None:
+                img = game.map[y][x].image
+                img = pygame.transform.smoothscale(img, (100, 100))
+                screen.blit(img, (((x - game.main_room[1]) * 100) + shift_x,
+                                          ((y - game.main_room[0]) * 100) + shift_y))
+
+    card_draw()
+
+
 
 
 if __name__ == "__main__":
